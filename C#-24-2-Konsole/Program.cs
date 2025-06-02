9a8d9aefdb5d1bcf184cd11341da943095dffa92
StAland <steffen.aland@gmx.de>
2025-06-02 11:14:50 +0200

    funktionen

--- C#-24-2-Konsole/Program.cs
+++ C#-24-2-Konsole/Program.cs
@@ -25,26 +25,54 @@ namespace C__24_2_Konsole
 
             for (int i = 0; i < 100; i++)
             {
+                if (i % 7 == 0)
+                {
+                    continue;
+                }
+                if(i > 69)
+                {
+                    break;
+                }
                 Console.WriteLine(i);
             }
 
-            for (int i = 0;i < args.Length; i++)
+            //for (int i = 0;i < args.Length; i++)
+            //{
+            //    Console.WriteLine(args[i]);
+            //}
+            //Console.ReadLine();
+
+            //var liste = new List<string>();
+            //liste.Add("Hallo");
+            //liste.Add("Steffen");
+
+
+            //foreach (string s in liste)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            //liste.ForEach(x => Console.WriteLine(x));
+            Addieren(5, 7);
+        }
+
+        public static int Addieren(int a , int b)
+        {
+            return a + b;
+        }
+
+        public static int Addieren(List<int> ints)
+        {
+            if (ints.Count > 0)
             {
-                Console.WriteLine(args[i]);
+                var ergebnis = 0;
+                foreach (int i in ints)
+                {
+                    ergebnis += i;
+                }
+                return ergebnis;
             }
-            Console.ReadLine();
-
-            var liste = new List<string>();
-            liste.Add("Hallo");
-            liste.Add("Steffen");
-
-
-            foreach (string s in liste)
-            {
-                Console.WriteLine(s);
-            }
-
-            liste.ForEach(x => Console.WriteLine(x));
+            return 0;
         }
 
     }
