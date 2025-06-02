2880eb863353d01c80059780adf76e7db6c93bd9
StAland <steffen.aland@gmx.de>
2025-06-02 09:58:22 +0200

    IF Else

--- C#-24-2-Konsole/Program.cs
+++ C#-24-2-Konsole/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using C__24_2_Konsole;
+
+var password = "password";
+Console.WriteLine("Bitte Passwort eingeben: ");
+var eingabe = Console.ReadLine();
+if (eingabe == password)
+{
+    Console.WriteLine("Das Passwort ist korrekt");
+}
+else if (eingabe != "")
+{
+    Console.WriteLine("Bitte ein Passwort eingeben");
+}
+else
+{
+    Console.WriteLine("Das Passwort stimmt nicht");
+}
