4df9f0373cbcb94110c0d829e9451135b6709183
StAland <steffen.aland@gmx.de>
2025-06-10 08:37:02 +0200

    Klassen, Setter und Getter

---
+++ C#-24-2-Konsole/Haus.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__24_2_Konsole
+{
+    internal class Haus
+    {
+        private string _farbe;
+        private int _anzahlZimmer;
+
+        internal Haus(string farbe, int anzahlZimmer)
+        {
+            _farbe = farbe;
+            _anzahlZimmer = anzahlZimmer;
+        }
+
+        public string getFarbe()
+        {
+            return _farbe;
+        }
+
+        public void setFarbe(string farbe)
+        {
+            string[] erlaubteFarben = ["gruen", "gelb", "blau"];
+            if (erlaubteFarben.Contains(farbe))
+            {
+                _farbe = farbe;
+            }
+        }
+
+        internal void Konselenausgabe()
+        {
+            Console.WriteLine($"Das Haus hat {_anzahlZimmer} Zimmer und ist {_farbe}");
+        }
+    }
+}
--- C#-24-2-Konsole/Program.cs
+++ C#-24-2-Konsole/Program.cs
@@ -9,71 +9,9 @@ namespace C__24_2_Konsole
 
         static void Main(string[] args)
         {
-            //var zaehler = 0;
-            //while (zaehler < 0)
-            //{
-            //    Console.WriteLine("Kopf: " + zaehler);
-            //    zaehler++;
-            //}
-
-            //zaehler = 0;
-            //do
-            //{
-            //    Console.WriteLine("Fuss: " + zaehler);
-            //    zaehler++;
-            //} while (zaehler < 0);
-
-            for (int i = 0; i < 100; i++)
-            {
-                if (i % 7 == 0)
-                {
-                    continue;
-                }
-                if(i > 69)
-                {
-                    break;
-                }
-                Console.WriteLine(i);
-            }
-
-            //for (int i = 0;i < args.Length; i++)
-            //{
-            //    Console.WriteLine(args[i]);
-            //}
-            //Console.ReadLine();
-
-            //var liste = new List<string>();
-            //liste.Add("Hallo");
-            //liste.Add("Steffen");
-
-
-            //foreach (string s in liste)
-            //{
-            //    Console.WriteLine(s);
-            //}
-
-            //liste.ForEach(x => Console.WriteLine(x));
-            Addieren(5, 7);
-        }
-
-        public static int Addieren(int a , int b)
-        {
-            return a + b;
-        }
-
-        public static int Addieren(List<int> ints)
-        {
-            if (ints.Count > 0)
-            {
-                var ergebnis = 0;
-                foreach (int i in ints)
-                {
-                    ergebnis += i;
-                }
-                return ergebnis;
-            }
-            return 0;
-        }
+            var haus = new Haus("weiß", 4);
+            haus.Konselenausgabe();
+        }       
 
     }
 }
